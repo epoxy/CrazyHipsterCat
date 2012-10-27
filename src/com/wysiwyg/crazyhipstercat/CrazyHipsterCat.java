48c943f4c440e49e7de01eba79524c2afcd1aaa4
epoxy <palmqvist.anton@gmail.com>
2012-10-27 22:10:50 +0200

    Made background change on touch down

--- src/com/wysiwyg/crazyhipstercat/CrazyHipsterCat.java
+++ src/com/wysiwyg/crazyhipstercat/CrazyHipsterCat.java
@@ -1,15 +1,25 @@
 package com.wysiwyg.crazyhipstercat;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.RelativeLayout;
 
-public class CrazyHipsterCat extends Activity {
+public class CrazyHipsterCat extends Activity implements OnTouchListener{
 
+	private RelativeLayout relativeLayout;
+	private MediaPlayer mp;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.crazy_hipster_cat_activity);
+        relativeLayout=(RelativeLayout) findViewById(R.id.relative_layout);
+        relativeLayout.setBackgroundResource(R.drawable.cat);
+        relativeLayout.setOnTouchListener(this);
     }
 
     @Override
@@ -17,4 +27,15 @@ public class CrazyHipsterCat extends Activity {
         getMenuInflater().inflate(R.menu.crazy_hipster_cat_activity, menu);
         return true;
     }
+
+	public boolean onTouch(View v, MotionEvent event) {
+		if (event.getAction() == MotionEvent.ACTION_DOWN){
+			relativeLayout.setBackgroundResource(R.drawable.cat2);
+			
+		}
+		if (event.getAction() == MotionEvent.ACTION_UP){
+			relativeLayout.setBackgroundResource(R.drawable.cat);
+		}
+		return true;
+	}
 }
